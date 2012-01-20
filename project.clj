d01fe372a9f398ebfc0ca60ce0121b1352c790fc
Julian Gamble <mail@juliangamble.com>
2012-01-20 22:35:51 +1100

    Latest updates

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject ants "0.1"
     :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-    :jvm-opts ["-Djava.security.policy=example.policy"]
     :main demo.AntsApplet2)
 ;   :main applet)
     
